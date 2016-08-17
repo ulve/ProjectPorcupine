e511727f8eda36df7f39799e833d11f89a85476a
Olov Johansson <johansson.olov@gmail.com>
2016-08-17 22:08:08 +0200

    ping

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,6 +6,7 @@
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 
 # perhaps a way to always get latest or else this has to be bumped every time a new version of unity is released
+#come on travis
 echo 'Downloading Unity-5.2.5f1.pkg: '
 curl -o Unity.pkg http://download.unity3d.com/download_unity/ad2d0368e248/MacEditorInstaller/Unity-5.2.5f1.pkg
 
