30a751c1ef185864be6ec6b1f7a4baae54e2e81a
Olov Johansson <johansson.olov@gmail.com>
2016-08-17 21:54:34 +0200

    try older version of unity

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,19 +3,43 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="project-porcupine"
+project="ci-build"
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity-win.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -quit
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
+  -logFile $(pwd)/unity-mac.log \
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
-echo 'Logs from build'
-cat $(pwd)/unity.log
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity-linux.log \
+  -projectPath $(pwd) \
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+  -quit
 
-#removed windows and linux builds. Perhaps they can be fixed
+echo 'Logs from windows build'
+cat $(pwd)/unity-win.log
+
+echo 'Logs from mac build'
+cat $(pwd)/unity-mac.log
+
+echo 'Logs from linux build'
+cat $(pwd)/unity-linux.log
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,8 +6,8 @@
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 
 # perhaps a way to always get latest or else this has to be bumped every time a new version of unity is released
-echo 'Downloading Unity-5.4.0f3: '
-curl -o Unity.pkg http://download.unity3d.com/download_unity/a6d8d714de6f/MacEditorInstaller/Unity-5.4.0f3.pkg
+echo 'Downloading Unity-5.2.5f1.pkg: '
+curl -o Unity.pkg http://download.unity3d.com/download_unity/ad2d0368e248/MacEditorInstaller/Unity-5.2.5f1.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
